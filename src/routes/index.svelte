e14b8dd47e525e4803de1e1226da67e03fff6b0e
Mathieu <mathieux51@gmail.com>
2019-07-17 07:42:16 +0200

    Remove great succes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -38,15 +38,4 @@
   <title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-  <img alt="Borat" src="great-success.png" />
-  <figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p>
-  <strong>
-    Try editing this file (src/routes/index.svelte) to test live reloading.
-  </strong>
-</p>
+<h1>Hello from Juliane</h1>
