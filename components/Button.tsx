2c3b11ecda682d129f951154668c2dde58086640
Mathieu <mathieux51@gmail.com>
2020-01-06 08:09:20 +0100

    Add type to Buttom component

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,12 +5,24 @@ const StyledButton = styled.button``
 
 type Props = {
   className?: string
-  onClick: (evt: React.MouseEvent<HTMLElement>) => void
+  onClick?: (evt: React.MouseEvent<HTMLElement>) => void
   onBlur?: (evt: React.FocusEvent<HTMLButtonElement>) => void
+  type?: 'button' | 'submit' | 'reset' | undefined
 }
 
-const Button: React.FC<Props> = ({ className, children, onClick, onBlur }) => (
-  <StyledButton className={className} onClick={onClick} onBlur={onBlur}>
+const Button: React.FC<Props> = ({
+  className,
+  children,
+  onClick,
+  onBlur,
+  type
+}) => (
+  <StyledButton
+    className={className}
+    onClick={onClick}
+    onBlur={onBlur}
+    type={type}
+  >
     {children}
   </StyledButton>
 )
