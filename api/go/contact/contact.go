35a20d49ef8c5046a8687b3955ba39bb198d5883
Mathieu <mathieux51@gmail.com>
2020-01-06 14:08:01 +0100

    WIP contact API

--- api/go/contact/contact.go
+++ api/go/contact/contact.go
@@ -1,12 +1,44 @@
-package main
+package contact
 
 import (
 	"fmt"
 	"net/http"
-	"time"
+	"net/smtp"
 )
 
-// Handler ...
+// Handler sends an email and returns 201 if ok
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, time.Now().Format(time.RFC1123Z))
+	// Sender data
+	from := "mathieux51@gmail.com"
+	password := "MySecretPassword"
+	// Receiver email address
+	to := []string{
+		"mathieux51@gmail.com",
+	}
+	// smtp server configuration
+	host := "smtp.gmail.com"
+	port := "587"
+	address := host + ":" + port
+
+	// Message
+	message := []byte("This is a really unimaginative message, I know.")
+	// Authentication
+	auth := smtp.PlainAuth("", from, password, host)
+	// Sending email
+	err := smtp.SendMail(address, auth, from, to, message)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Set("Content-Type", "application/json")
+
+		// io.WriteString(w, `{"status": "Internal Server Error"}`)
+
+		return
+	}
+	fmt.Println("Email Sent!")
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+
+	// io.WriteString(w, `{"status": "ok"}`)
 }
---
+++ api/go/contact/contact_test.go
@@ -0,0 +1,30 @@
+package contact
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestContactHandler(t *testing.T) {
+	req, err := http.NewRequest("POST", "/contact", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Handler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"alive": true}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
