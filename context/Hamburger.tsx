6432a3fcd9eae118c156ac08edefb384835668c9
Mathieu <mathieux51@gmail.com>
2020-03-07 21:49:29 +0100

    Add Hamburger context

---
+++ context/Hamburger.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const HamburgerContext: React.Context<any> = React.createContext(null)
+HamburgerContext.displayName = 'HamburgerContext'
+const { Provider, Consumer } = HamburgerContext
+
+export { Consumer as HamburgerConsuer }
+
+type Props = {
+  children: React.ReactNode
+}
+
+export function HamburgerProvider(props: Props) {
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  const state = {
+    isOpen,
+    setIsOpen,
+  }
+
+  return <Provider value={state}>{props.children}</Provider>
+}
+
