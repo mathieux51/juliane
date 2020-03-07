f4ee91bb21d3e8098852b0cc8ca13e2062c9c983
Mathieu <mathieux51@gmail.com>
2020-03-07 21:12:51 +0100

    Update package, fix redirect and add deploy script

---
+++ scripts/deploy-dev.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+npx now ls julianehendershot > temp
+ALIAS=$(cat temp | grep julianehendershot | awk '{ print $2 }' | head -1)
+npx now alias $ALIAS dev.julianehendershot.com
+rm temp
