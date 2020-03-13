b3de4d613cc6edf446978e0a515c0179650f17f8
Mathieu <mathieux51@gmail.com>
2020-03-13 07:39:51 +0100

    Update deploy dev script

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -3,6 +3,7 @@
 set -o errexit
 set -o nounset
 
+npx now
 npx now ls julianehendershot > temp
 ALIAS=$(cat temp | grep julianehendershot | awk '{ print $2 }' | head -1)
 npx now alias $ALIAS dev.julianehendershot.com
