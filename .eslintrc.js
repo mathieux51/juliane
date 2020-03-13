f74b2edc9cc09207bc4c0a1d0458b35ff9c21102
Mathieu <mathieux51@gmail.com>
2020-03-13 21:16:09 +0100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,33 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-    'node': true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true
   },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-    'prettier',
-  ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
+  extends: ['plugin:react/recommended', 'google', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    'ecmaVersion': 2018,
-    'sourceType': 'module',
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
-  'plugins': [
-    'react',
-    '@typescript-eslint',
-    'prettier',
-  ],
-  'rules': {
-     "prettier/prettier": "error",
-    "semi": 0,
-    "react/prop-types": 0,
-    "react/display-name": 0,
-    "require-jsdoc": "warn"
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    semi: 0,
+    'react/prop-types': 0,
+    'react/display-name': 0,
+    'require-jsdoc': 'warn'
   },
   settings: {
     react: {
-      version: "detect"
+      version: 'detect'
     }
-  },
-};
+  }
+}
