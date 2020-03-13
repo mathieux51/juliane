4ff0f5d7b2eb34711965c73d85430cb791f97fe0
Mathieu <mathieux51@gmail.com>
2020-03-13 21:16:46 +0100

    Fix linting issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import { NextPageContext } from 'next'
 import 'lite-youtube-embed/src/lite-yt-embed.css'
 import 'lite-youtube-embed/src/lite-yt-embed.js'
 import { languageFromContext } from '../helpers/helpers'
@@ -9,17 +8,6 @@ import { isServer } from '../helpers/helpers'
 const Container = styled.div`
   width: 100%;
 `
-
-// const Box = styled.div`
-//   width: 100%;
-//   height: 300px;
-//   border-radius: 8px;
-//   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-//   transition: all 0.4s cubic-bezier(0.25, 0.8, 0.25, 1);
-//   &:hover {
-//     box-shadow: 0 7px 10px rgba(0, 0, 0, 0.24), 0 5px 5px rgba(0, 0, 0, 0.12);
-//   }
-// `
 
 const ColorBox = styled.div`
   width: 100%;
