7f821472c668de755818f6c06f814947314aaf15
Mathieu <mathieux51@gmail.com>
2020-03-15 13:22:25 +0100

    Add ZEIT_TOKEN to deploy script

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -3,8 +3,18 @@
 set -o errexit
 set -o nounset
 
-npx now --no-clipboard -t ${ZEIT_TOKEN} -m commit=${GITHUB_SHA} -m branch=${GITHUB_REF}
-# npx now --no-clipboard -t ${ZEIT_TOKEN}
+if [[ -z "$NOW_PROJECT_ID" ]]; then
+  echo "Missing NOW_PROJECT_ID" 1>&2
+  exit 1
+fi
+
+if [[ -z "$NOW_ORG_ID" ]]; then
+  echo "Missing NOW_ORG_ID" 1>&2
+  exit 1
+fi
+
+npx now --no-clipboard -t ${NOW_TOKEN} -m commit=${GITHUB_SHA} -m branch=${GITHUB_REF}
+# npx now --no-clipboard -t ${NOW_TOKEN}
 npx now ls julianehendershot > temp
 ALIAS=$(cat temp | grep julianehendershot | awk '{ print $2 }' | head -1)
 npx now alias $ALIAS dev.julianehendershot.com
