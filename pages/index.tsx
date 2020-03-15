0ac4e6b865ae19e602961f664054f48aa17f1cd1
Mathieu <mathieux51@gmail.com>
2020-03-15 08:48:47 +0100

    Remove lite-youtube-embed related code from pages/index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import 'lite-youtube-embed/src/lite-yt-embed.css'
-import 'lite-youtube-embed/src/lite-yt-embed.js'
+import { NextPageContext } from 'next'
 import { languageFromContext } from '../helpers/helpers'
-import { isServer } from '../helpers/helpers'
 
 const Container = styled.div`
   width: 100%;
@@ -39,24 +37,9 @@ export default class extends React.PureComponent {
   }
 
   render() {
-    const liteYoutubeCSS = {
-      backgroundImage: "url('https://i.ytimg.com/vi/ogfYd705cRs/hqdefault.jpg'",
-      width: '100%',
-      height: '100%'
-    }
     return (
       <Container>
-        <GreyBox>
-          {!isServer && (
-            <lite-youtube
-              videoid='b05Scfhi0dU'
-              style={liteYoutubeCSS}
-              className={liteYoutubeCSS}
-            >
-              <div className='lty-playbtn'></div>
-            </lite-youtube>
-          )}
-        </GreyBox>
+        <GreyBox></GreyBox>
         <OrangeBox>After effect (on demand)</OrangeBox>
         <RedBox>Post card USA</RedBox>
         <TurquoiseBox />
