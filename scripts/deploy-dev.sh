05a7bc9c4b28281f6e366c02224ff125a7d75438
Mathieu <mathieux51@gmail.com>
2020-03-15 08:51:45 +0100

    Update deploy dev script with zeit token

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -3,7 +3,8 @@
 set -o errexit
 set -o nounset
 
-npx now
+npx now --no-clipboard -t ${ZEIT_TOKEN} -m commit=${GITHUB_SHA} -m branch=${GITHUB_REF}
+# npx now --no-clipboard -t ${ZEIT_TOKEN}
 npx now ls julianehendershot > temp
 ALIAS=$(cat temp | grep julianehendershot | awk '{ print $2 }' | head -1)
 npx now alias $ALIAS dev.julianehendershot.com
