85d0a1afb8953136e680c8c4d1b71090203e3c72
Mathieu <mathieux51@gmail.com>
2020-03-15 14:00:57 +0100

    Fix Eliminate render-blocking resources

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Head from 'next/head'
 
-const _Head = () => (
-  <Head>
-    <title>julianehendershot.com</title>
-    <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    {/* <link rel='preconnect' href='https://www.google-analytics.com' />
+const _Head = () => {
+  const linkRef = React.useRef<HTMLLinkElement>(null)
+  const handleOnLoad = () => {
+    if (linkRef && linkRef.current) {
+      linkRef.current.onload = null
+      linkRef.current.rel = 'stylesheet'
+    }
+  }
+
+  return (
+    <Head>
+      <title>julianehendershot.com</title>
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      {/* <link rel='preconnect' href='https://www.google-analytics.com' />
     <link rel='preconnect' href='https://in.hotjar.com' />
     <link rel='preconnect' href='https://vars.hotjar.com' />
     <link rel='preconnect' href='https://vc.hotjar.io' />
@@ -33,13 +42,24 @@ const _Head = () => (
       href='/static/safari-pinned-tab.svg'
       color='#5bbad5'
     /> */}
-    {/* <meta name='msapplication-TileColor' content='#da532c' /> */}
-    {/* <meta name='theme-color' content='#ffffff' /> */}
-    <link
-      href='https://fonts.googleapis.com/css?family=Montserrat&display=swap'
-      rel='stylesheet'
-    />
-  </Head>
-)
+      {/* <meta name='msapplication-TileColor' content='#da532c' /> */}
+      {/* <meta name='theme-color' content='#ffffff' /> */}
+      <link
+        ref={linkRef}
+        rel='preload'
+        href='https://fonts.googleapis.com/css?family=Montserrat&display=swap'
+        as='style'
+        onLoad={handleOnLoad}
+      />
+      <noscript>
+        <link
+          href='https://fonts.googleapis.com/css?family=Montserrat&display=swap'
+          rel='stylesheet'
+          type='text/css'
+        />
+      </noscript>
+    </Head>
+  )
+}
 
 export default _Head
