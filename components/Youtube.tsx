68747138579fe06a695fd4086df6a6203660db10
Mathieu <mathieux51@gmail.com>
2020-03-20 08:56:19 +0100

    Update Youtube component

--- components/Youtube.tsx
+++ components/Youtube.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 import { isServer } from '../helpers/helpers'
 
+const width = '560px'
+const height = '315px'
+
 const Placeholder = styled.div.attrs({ className: 'flex jc-c ai-c' })`
-  width: 560px;
-  height: 315px;
+  width: ${width};
+  height: ${height};
 `
 
 const Iframe = styled.iframe`
+  width: ${width};
+  height: ${height};
   & + ${Placeholder} {
     display: none;
   }
@@ -16,13 +21,11 @@ type Props = {
   className?: string
 }
 
-const Youtube: React.FC<Props> = () => (
+const Youtube: React.FC<Props> = ({ className }) => (
   <>
     {!isServer && (
       <Iframe
-        className='lazyload'
-        width='560'
-        height='315'
+        className={`${className} lazyload`}
         data-src='https://www.youtube-nocookie.com/embed/IMk_qD-dtqE'
         frameBorder='0'
         allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
