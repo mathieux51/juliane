81234d32b738999ad646cf4e4f73c1403c2d2a28
Mathieu <mathieux51@gmail.com>
2020-03-20 09:25:47 +0100

    Remove debug.tsx

--- pages/en/debug.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { useRouter } from 'next/router'
-
-function Index() {
-  const router = useRouter()
-  return <pre>{JSON.stringify(router, null, 2)}</pre>
-}
-
-export default Index
