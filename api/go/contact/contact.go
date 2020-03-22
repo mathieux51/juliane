8c51e598bf0c7ac0dfe58da525d95f67f52b7330
Mathieu <mathieux51@gmail.com>
2020-03-22 18:23:36 +0100

    Update contact API

--- api/go/contact/contact.go
+++ api/go/contact/contact.go
@@ -1,40 +1,45 @@
 package contact
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"net/smtp"
 )
 
 // Handler sends an email and returns 201 if ok
 func Handler(w http.ResponseWriter, r *http.Request) {
 	// Sender data
-	// from := "mathieux51@gmail.com"
-	// password := "MySecretPassword"
-	// // Receiver email address
-	// to := []string{
-	// 	"mathieux51@gmail.com",
-	// }
+	from := "jmhenderschmidt@gmail.com"
+	password := "cErVEzA2323"
+	// Receiver email address
+	to := []string{
+		"jmhenderschmidt@gmail.com",
+	}
 	// // smtp server configuration
-	// host := "smtp.gmail.com"
-	// port := "587"
-	// address := host + ":" + port
+	host := "smtp.gmail.com"
+	port := "587"
+	address := host + ":" + port
 
-	// // Message
-	// message := []byte("This is a really unimaginative message, I know.")
-	// // Authentication
-	// auth := smtp.PlainAuth("", from, password, host)
-	// // Sending email
-	// err := smtp.SendMail(address, auth, from, to, message)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	w.Header().Set("Content-Type", "application/json")
+	// Message
+	msg := []byte("To: recipient@example.net\r\n" +
+		"Subject: discount Gophers!\r\n" +
+		"\r\n" +
+		"This is the email body.\r\n")
+	// Authentication
+	auth := smtp.PlainAuth("", from, password, host)
+	// Sending email
+	err := smtp.SendMail(address, auth, from, to, msg)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Set("Content-Type", "application/json")
 
-	// 	// io.WriteString(w, `{"status": "Internal Server Error"}`)
+		io.WriteString(w, `{"status": "Internal Server Error"}`)
 
-	// 	return
-	// }
-	// fmt.Println("Email Sent!")
+		return
+	}
+	fmt.Println("Email Sent!")
 
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
