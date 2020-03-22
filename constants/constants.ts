1f78cad8807c6a8cd61c407e4e4d183be22294a7
Mathieu <mathieux51@gmail.com>
2020-03-22 14:46:15 +0100

    Add RECAPTCHA_CLIENT_SIDE to publicRuntimeConfig

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,3 +1,7 @@
+import getConfig from 'next/config'
+
+const { publicRuntimeConfig } = getConfig()
+
 export const isProd = process.env.NODE_ENV === 'production'
 
 export const isDev = !isProd
@@ -6,6 +10,6 @@ export const defaultLanguage = 'en'
 
 export const config = {
   RECAPTCHA_CLIENT_SIDE: isProd
-    ? process.env.RECAPTCHA_CLIENT_SIDE
+    ? publicRuntimeConfig.RECAPTCHA_CLIENT_SIDE
     : '6LdGEuMUAAAAACFNXOljhnX9AmsO4OKXaayowpqF'
 }
--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 const withCSS = require('@zeit/next-css')
 
-module.exports = withCSS({})
+module.exports = withCSS({
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    RECAPTCHA_CLIENT_SIDE: process.env.RECAPTCHA_CLIENT_SIDE
+  }
+})
