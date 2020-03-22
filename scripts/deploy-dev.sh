c3f6cd206efef111f66db226b6874b6a53ab8a20
Mathieu <mathieux51@gmail.com>
2020-03-22 14:22:36 +0100

    Add _test.go to nowignore

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -3,8 +3,6 @@
 set -o errexit
 set -o nounset
 
-printenv
-
 if [[ -z "$NOW_PROJECT_ID" ]]; then
   echo "Missing NOW_PROJECT_ID" 1>&2
   exit 1
@@ -19,6 +17,9 @@ if [[ -z "$RECAPTCHA_CLIENT_SIDE" ]]; then
   echo "Missing RECAPTCHA_CLIENT_SIDE" 1>&2
   exit 1
 fi
+
+npx now -t ${NOW_TOKEN} secrets rm RECAPTCHA_CLIENT_SIDE -y
+npx now -t ${NOW_TOKEN} secrets add RECAPTCHA_CLIENT_SIDE ${RECAPTCHA_CLIENT_SIDE}
 
 npx now --no-clipboard -t ${NOW_TOKEN} -e RECAPTCHA_CLIENT_SIDE=${RECAPTCHA_CLIENT_SIDE} -m commit=${GITHUB_SHA} -m branch=${GITHUB_REF}
 # npx now --no-clipboard -t ${NOW_TOKEN}
