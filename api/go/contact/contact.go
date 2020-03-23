e89291aa1f928153d20ab05db8326030e72fb300
Mathieu <mathieux51@gmail.com>
2020-03-23 07:25:04 +0100

    Replace Handler function by health check

--- api/go/contact/contact.go
+++ api/go/contact/contact.go
@@ -1,48 +1,54 @@
 package contact
 
 import (
-	"fmt"
 	"io"
 	"net/http"
-	"net/smtp"
 )
 
-// Handler sends an email and returns 201 if ok
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// Sender data
-	from := "jmhenderschmidt@gmail.com"
-	password := "cErVEzA2323"
-	// Receiver email address
-	to := []string{
-		"jmhenderschmidt@gmail.com",
-	}
-	// // smtp server configuration
-	host := "smtp.gmail.com"
-	port := "587"
-	address := host + ":" + port
-
-	// Message
-	msg := []byte("To: recipient@example.net\r\n" +
-		"Subject: discount Gophers!\r\n" +
-		"\r\n" +
-		"This is the email body.\r\n")
-	// Authentication
-	auth := smtp.PlainAuth("", from, password, host)
-	// Sending email
-	err := smtp.SendMail(address, auth, from, to, msg)
-	if err != nil {
-		fmt.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Header().Set("Content-Type", "application/json")
-
-		io.WriteString(w, `{"status": "Internal Server Error"}`)
-
-		return
-	}
-	fmt.Println("Email Sent!")
-
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 
-	io.WriteString(w, `{"status": "ok"}`)
+	io.WriteString(w, `{"alive": true}`)
 }
+
+// Handler sends an email and returns 201 if ok
+// func Handler(w http.ResponseWriter, r *http.Request) {
+// 	// Sender data
+// 	from := "jmhenderschmidt@gmail.com"
+// 	password := "cErVEzA2323"
+// 	// Receiver email address
+// 	to := []string{
+// 		"jmhenderschmidt@gmail.com",
+// 	}
+// 	// // smtp server configuration
+// 	host := "smtp.gmail.com"
+// 	port := "587"
+// 	address := host + ":" + port
+//
+// 	// Message
+// 	msg := []byte(`To: jmhenderschmidt@gmail.com
+// Subject: AWESOME
+// This is the email body.
+// `)
+// 	// Authentication
+// 	auth := smtp.PlainAuth("", from, password, host)
+// 	// Sending email
+// 	fmt.Printf("Hander sending email: adress %v, auth %v, from %v, to %v, msm %v", address, auth, from, to, msg)
+// 	// err := smtp.SendMail(address, auth, from, to, msg)
+// 	// if err != nil {
+// 	// 	fmt.Println(err)
+// 	// 	w.WriteHeader(http.StatusInternalServerError)
+// 	// 	w.Header().Set("Content-Type", "application/json")
+//
+// 	// 	io.WriteString(w, `{"status": "Internal Server Error"}`)
+//
+// 	// 	return
+// 	// }
+// 	fmt.Println("Handler email sent")
+//
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Header().Set("Content-Type", "application/json")
+//
+// 	io.WriteString(w, `{"status": "ok"}`)
+// }
