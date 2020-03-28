0589a2db397eab400b6e8483c0ba4e482952434a
Mathieu <mathieux51@gmail.com>
2020-03-28 13:45:01 +0100

    Add hooks rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,12 +23,14 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
     semi: 0,
     'react/prop-types': 0,
     'react/display-name': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'require-jsdoc': 0,
     '@typescript-eslint/member-delimiter-style': [
       'error',
