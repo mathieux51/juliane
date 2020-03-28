9d844ed773542d7c56ea2b98876f8e7cc8c6faaa
Mathieu <mathieux51@gmail.com>
2020-03-28 15:16:08 +0100

    Reorganize go files so it works for now

--- api/go/email.go
+++ api/go/email.go
@@ -2,7 +2,9 @@ package email
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/smtp"
@@ -87,4 +89,41 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 	sendResponse(w, http.StatusOK)
 
+}
+
+// CheckGoogleCaptcha makes an API call to check the token
+func CheckGoogleCaptcha(token string) (bool, error) {
+	secret := os.Getenv("RECAPTCHA_SERVER_SIDE")
+	req, err := http.NewRequest("POST", "https://www.google.com/recaptcha/api/siteverify", nil)
+	q := req.URL.Query()
+	q.Add("secret", secret)
+	q.Add("response", token)
+	req.URL.RawQuery = q.Encode()
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false, err
+	}
+
+	var googleResponse map[string]interface{}
+	json.Unmarshal(b, &googleResponse)
+	log.Printf("%+v\n", googleResponse)
+	gr, ok := googleResponse["success"]
+	if !ok {
+		return false, errors.New("success not in Google response")
+	}
+
+	valid, ok := gr.(bool)
+	if !ok {
+		return false, errors.New("failed to assert success as bool")
+	}
+
+	return valid, nil
 }
--- api/go/email_test.go
+++ api/go/email_test.go
@@ -68,3 +68,23 @@ func TestHandler_ok(t *testing.T) {
 			rr.Body.String(), expected)
 	}
 }
+
+func TestCheckGoogleCaptcha(t *testing.T) {
+	const token = "03AHaCkAZ0p1eGYSOb0vhlE6ZHcpjTPYvvQOHxQ4yMirtgvol2wSUmdGi7AX6PJ__6G9BMiDKqx2yEdTFHFoDR7UcheWRFI-Q5QGIuMX1ocfOnKLIKJTRqKQQHNwVd6CfOaPgUJX528sP0mROvomgHEIwgV3Vs0sCGj4vdjsAHLo8EaNyCBkvj0yeV5R_SnOW4h52xZ0-GEe0z8jVVFwg-69MTPb9bpbKTjTc-e5Wq6eR3uWKYJtjMneZREW2kRbC-aiRRVmkvfqh4PNDlUeRUgdoP2XvZZbPb3UVCNVbnYNxckMh5m64qj9gNycpcstomQsILeVwVstnp64kYQuLy1slQGTw7SBgEAmrKHOZnJqux-zYQbD7IhEmR61gDjugGc_XqJi06K2MdEHTn1GFadWJKf6yYfXlcMtTeB7wRL0x6S7-ldaZ-RaQ"
+
+	_, err := CheckGoogleCaptcha(token)
+
+	// Check for errors
+	if err != nil {
+		t.Errorf("CheckGoogleCaptcha returned an error: got %v want %v",
+			err, nil)
+	}
+
+	// Token is only valid once
+	// Uncomment if test needed
+	// expected := true
+	// if !valid {
+	// 	t.Errorf("CheckGoogleCaptcha unexpected return: got %v want %v",
+	// 		valid, expected)
+	// }
+}
