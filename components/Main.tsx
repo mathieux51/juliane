97be80322e0658f35f6b7224c603b46eebe94566
Mathieu <mathieux51@gmail.com>
2020-03-30 08:22:58 +0200

    Add dynamic imports to Main

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,22 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
-import Section1 from './Section1'
-import Contact from './Contact'
-// const Vimeo = dynamic(() => import('./Vimeo'), {
-//   ssr: false
-// })
-// <Vimeo />
+import dynamic from 'next/dynamic'
+const Section1 = dynamic(() => import('./Section1'))
+const Contact = dynamic(() => import('./Contact'))
+
 const Container = styled.div`
   width: 100%;
 `
 
-function Main() {
-  return (
-    <Container>
-      <Section1 />
-      <Contact />
-    </Container>
-  )
-}
+const Main: React.FC = () => (
+  <Container>
+    <Section1 />
+    <Contact />
+  </Container>
+)
 
 export default Main
