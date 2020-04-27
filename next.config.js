c8b3f7ba0be8df414d263211db863ceef33fdcd5
Mathieu <mathieux51@gmail.com>
2020-04-27 15:50:40 +0200

    Add bundle analyser

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
-const withCSS = require('@zeit/next-css') // eslint-disable-line
-
-module.exports = withCSS({
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    RECAPTCHA_CLIENT_SIDE: process.env.RECAPTCHA_CLIENT_SIDE,
-  },
+/* eslint @typescript-eslint/no-var-requires: 0 */
+const withCSS = require('@zeit/next-css')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 })
+
+module.exports = withBundleAnalyzer(
+  withCSS({
+    publicRuntimeConfig: {
+      // Will be available on both server and client
+      RECAPTCHA_CLIENT_SIDE: process.env.RECAPTCHA_CLIENT_SIDE,
+    },
+  })
+)
