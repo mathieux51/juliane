78940136a3a99a83d3450f354114d682512809a8
Mathieu <mathieux51@gmail.com>
2020-04-28 08:15:14 +0200

    Fix Head props

--- components/Head.tsx
+++ components/Head.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import { useIntl } from 'react-intl'
 
-type Props = {
-  language: string
-}
+type Props = {}
 
 const _Head: React.FC<Props> = () => {
   const intl = useIntl()
