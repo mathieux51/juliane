fe628d58634065b299854c4068cea49c17a3c3e6
Mathieu <mathieux51@gmail.com>
2020-04-28 07:21:16 +0200

    Move Theme declaration

--- types/styled.d.ts
+++ types/styled.d.ts
@@ -1,9 +1,8 @@
 import 'styled-components'
 import theme from '../style/theme'
 
-type Theme = typeof theme
-
 declare module 'styled-components' {
+  type Theme = typeof theme
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends Theme {}
 }
