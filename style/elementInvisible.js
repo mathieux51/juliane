ce304d1bd3fe8aaf9eadf7b6136c6ebba9444c1d
Mathieu <mathieux51@gmail.com>
2020-04-29 12:00:15 +0200

    Add elementInvisible

---
+++ style/elementInvisible.js
@@ -0,0 +1,12 @@
+import { css } from 'styled-components'
+
+const elementInvisible = css`
+  position: absolute !important;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
+  clip: rect(1px, 1px, 1px, 1px);
+`
+
+export default elementInvisible
