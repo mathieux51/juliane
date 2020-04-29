d93a1f788f9be6bcb10f16c5e1a741edb2fc43c6
Mathieu <mathieux51@gmail.com>
2020-04-29 12:02:21 +0200

    Upgrade LanguagePicker with SEO friendly links

--- components/LanguagePicker.tsx
+++ components/LanguagePicker.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { FlattenSimpleInterpolation } from 'styled-components'
 import Button from './Button'
 import EnglishFlag from '../components/EnglishFlag'
 import FrenchFlag from '../components/FrenchFlag'
 import GermanFlag from '../components/GermanFlag'
 import Earth from './Earth'
+import useClickOutside from '../hooks/useClickOutside'
+import elementInvisible from '../style/elementInvisible'
 
 const Container = styled.div``
 
@@ -21,6 +23,8 @@ const Ul = styled.ul.attrs({ className: 'absolute' })`
   margin: 0;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   background: white;
+  ${({ isOpen }: { isOpen: boolean }): string | FlattenSimpleInterpolation =>
+    isOpen ? '' : elementInvisible}
 `
 
 const Li = styled.li``
@@ -38,34 +42,40 @@ type Props = {
 }
 
 const LanguagePicker: React.FC<Props> = ({ className }) => {
+  const buttonRef: React.RefObject<HTMLButtonElement> = React.createRef()
   const [isOpen, setIsOpen] = React.useState(false)
 
-  const handle = () => setIsOpen(!isOpen)
+  const handle = (): void => setIsOpen(!isOpen)
+  const handleClose = (): void => {
+    if (!isOpen) {
+      return
+    }
+    setIsOpen(false)
+  }
+  useClickOutside(buttonRef, handleClose)
 
   return (
     <Container className={`relative ${className}`}>
-      <Button onClick={handle} aria-label='language picker'>
+      <Button onClick={handle} aria-label='language picker' ref={buttonRef}>
         <StyledEarth />
       </Button>
-      {isOpen && (
-        <Ul>
-          <Li>
-            <A href='/en'>
-              English <EnglishFlag />
-            </A>
-          </Li>
-          <Li>
-            <A href='/fr'>
-              French <FrenchFlag />
-            </A>
-          </Li>
-          <Li>
-            <A href='/de'>
-              German <GermanFlag />
-            </A>
-          </Li>
-        </Ul>
-      )}
+      <Ul isOpen={isOpen}>
+        <Li>
+          <A href='/en'>
+            English <EnglishFlag />
+          </A>
+        </Li>
+        <Li>
+          <A href='/fr'>
+            French <FrenchFlag />
+          </A>
+        </Li>
+        <Li>
+          <A href='/de'>
+            German <GermanFlag />
+          </A>
+        </Li>
+      </Ul>
     </Container>
   )
 }
