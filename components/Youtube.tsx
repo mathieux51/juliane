7a4a3ce8733a17ca390f5c9568354d87b5c3495d
Mathieu <mathieux51@gmail.com>
2022-04-09 15:38:52 +0200

    update youtube component

--- components/Youtube.tsx
+++ components/Youtube.tsx
@@ -1,54 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 import LiteYouTubeEmbed from 'react-lite-youtube-embed'
-
-type mapPropsToLiteYouTubeEmbedProps = {
-  videoID: string
-  className?: string
-  adNetwork?: boolean
-  playlist?: boolean
-  poster?: string
-  title?: string
-  activatedClass?: string
-  iframeClass?: string
-  playerClass?: string
-  wrapperClass?: string
-}
-
-const MapPropsToLiteYouTubeEmbed: React.FC<mapPropsToLiteYouTubeEmbedProps> = ({
-  videoID,
-  className,
-  adNetwork,
-  playlist,
-  poster,
-  title,
-  activatedClass,
-  playerClass,
-  iframeClass,
-}) => (
-  <LiteYouTubeEmbed
-    id={videoID}
-    wrapperClass={(className ? className : '') + ' yt-lite'}
-    adNetwork={adNetwork}
-    playlist={playlist}
-    poster={poster}
-    title={title}
-    activatedClass={activatedClass}
-    iframeClass={iframeClass}
-    playerClass={playerClass}
-  />
-)
-
-const StyledLiteYouTubeEmbed = styled(MapPropsToLiteYouTubeEmbed)`
-  background-repeat: no-repeat;
-  background-size: contain;
-  width: 100%;
-  height: 100%;
-  & iframe {
-    width: 100%;
-    height: 100%;
-  }
-`
 
 type YoutubeProps = {
   className?: string
@@ -59,9 +11,9 @@ const Youtube: React.FC<YoutubeProps> = ({ className }) => {
   const title = 'Test'
 
   return (
-    <StyledLiteYouTubeEmbed
-      videoID={videoID}
-      className={className}
+    <LiteYouTubeEmbed
+      id={videoID}
+      wrapperClass={(className ? className : '') + ' yt-lite'}
       title={title}
     />
   )
