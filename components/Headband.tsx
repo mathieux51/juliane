2d4b134a85e24ae6c9d658c0901eb8455f1cf771
Mathieu <mathieux51@gmail.com>
2025-06-20 13:59:09 +0200

    update style

--- components/Headband.tsx
+++ components/Headband.tsx
@@ -14,13 +14,20 @@ const Container = styled.section`
   width: 100%;
 `
 
+const TextContainer = styled.div`
+  max-width: 978px;
+  width: 100%;
+`
+
 const JobText = styled.p`
+  color: ${({ theme }) => theme.white};
   font-size: 16px;
   letter-spacing: 0.05em;
   margin: 0;
 `
 
 const NameText = styled.h1`
+  color: ${({ theme }) => theme.white};
   font-size: 36px;
   font-weight: 700;
   letter-spacing: 0.15em;
@@ -29,6 +36,7 @@ const NameText = styled.h1`
 `
 
 const LocationText = styled.p`
+  color: ${({ theme }) => theme.white};
   font-size: 14px;
   letter-spacing: 0.05em;
   margin: 0;
@@ -42,9 +50,11 @@ const Headband: React.FC<Props> = ({ className }) => {
   const intl = useIntl()
   return (
     <Container className={className}>
-      <LocationText>{intl.formatMessage({ id: 'berlinBased' })}</LocationText>
-      <NameText>JULIANE HENDERSHOT</NameText>
-      <JobText> {intl.formatMessage({ id: 'jobs' })}</JobText>
+      <TextContainer>
+        <LocationText>{intl.formatMessage({ id: 'berlinBased' })}</LocationText>
+        <NameText>JULIANE HENDERSHOT</NameText>
+        <JobText> {intl.formatMessage({ id: 'jobs' })}</JobText>
+      </TextContainer>
     </Container>
   )
 }
